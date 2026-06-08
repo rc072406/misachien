9789005ba0c18e3987c7a03a776c15f85e92ec29
rc.github.io <rachelcheniphone@gmail.com>
2026-06-08 17:24:46 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://misachien.pages.dev',
-  base: '/',
+  site: 'https://rc072406.github.io',
+  base: '/misachien/',
 })
